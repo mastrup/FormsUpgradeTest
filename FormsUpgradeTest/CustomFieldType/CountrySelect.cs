2297d661072d4d23c67c54011f1d27c8881781ec
Søren Mastrup <soren.mastrup@danfoss.com>
2025-01-21 14:54:42 +0100

    Upgrade Umbraco to v13

--- FormsUpgradeTest/CustomFieldType/CountrySelect.cs
+++ FormsUpgradeTest/CustomFieldType/CountrySelect.cs
@@ -6,11 +6,6 @@ namespace FormsUpgradeTest.CustomFieldType
 {
     public class CountrySelect : FieldType
     {
-        private static readonly List<Setting> SettingsList = new List<Setting>
-        {
-            new LabelSettingAttribute("Enter value for country label"),
-        };
-
         public CountrySelect()
         {
             this.Id = new Guid("38b7ea54-e471-4bc8-a976-63a73d943bec");
@@ -21,6 +16,7 @@ namespace FormsUpgradeTest.CustomFieldType
             this.FieldTypeViewName = "FieldType.CountrySelect.cshtml";
         }
 
-        public override Dictionary<string, Setting> Settings() => SettingsList.ToDictionary(l => l.Alias, l => l);
+        [Setting("Label", Alias = "label", Description = "Enter value for country label", View = "textfield")]
+        public string? Label { get; set; }
     }
 }
--- FormsUpgradeTest/CustomFieldType/LabelSettingAttribute.cs
+++ FormsUpgradeTest/CustomFieldType/LabelSettingAttribute.cs
@@ -2,17 +2,17 @@
 
 namespace FormsUpgradeTest.CustomFieldType
 {
-    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
-    public class LabelSettingAttribute : Setting
-    {
-        public const string FieldAlias = "label";
+    //[AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    //public class LabelSettingAttribute : Setting
+    //{
+    //    public const string FieldAlias = "label";
 
-        public LabelSettingAttribute(string description)
-            : base("Label")
-        {
-            this.Description = description;
-            this.View = "textfield";
-            this.Alias = FieldAlias;
-        }
-    }
+    //    public LabelSettingAttribute(string description)
+    //        : base("Label")
+    //    {
+    //        this.Description = description;
+    //        this.View = "textfield";
+    //        this.Alias = FieldAlias;
+    //    }
+    //}
 }
