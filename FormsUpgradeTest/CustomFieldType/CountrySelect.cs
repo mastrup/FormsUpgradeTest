aa6fbd332c2dc8e9c983a136b0884eaa3aa244ac
Søren Mastrup <soren.mastrup@danfoss.com>
2025-01-21 14:46:23 +0100

    Added Umbraco

---
+++ FormsUpgradeTest/CustomFieldType/CountrySelect.cs
@@ -0,0 +1,26 @@
+﻿using Umbraco.Forms.Core;
+using Umbraco.Forms.Core.Attributes;
+using Umbraco.Forms.Core.Enums;
+
+namespace FormsUpgradeTest.CustomFieldType
+{
+    public class CountrySelect : FieldType
+    {
+        private static readonly List<Setting> SettingsList = new List<Setting>
+        {
+            new LabelSettingAttribute("Enter value for country label"),
+        };
+
+        public CountrySelect()
+        {
+            this.Id = new Guid("38b7ea54-e471-4bc8-a976-63a73d943bec");
+            this.Name = "Country DropdownList";
+            this.Description = "Render country dropdown list.";
+            this.DataType = FieldDataType.String;
+            this.Icon = "icon-globe";
+            this.FieldTypeViewName = "FieldType.CountrySelect.cshtml";
+        }
+
+        public override Dictionary<string, Setting> Settings() => SettingsList.ToDictionary(l => l.Alias, l => l);
+    }
+}
---
+++ FormsUpgradeTest/CustomFieldType/LabelSettingAttribute.cs
@@ -0,0 +1,18 @@
+﻿using Umbraco.Forms.Core.Attributes;
+
+namespace FormsUpgradeTest.CustomFieldType
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    public class LabelSettingAttribute : Setting
+    {
+        public const string FieldAlias = "label";
+
+        public LabelSettingAttribute(string description)
+            : base("Label")
+        {
+            this.Description = description;
+            this.View = "textfield";
+            this.Alias = FieldAlias;
+        }
+    }
+}
